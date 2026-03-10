c5e22cfe62cdddf6aa67873fd5034932b9f4e5ec
PHANUELF <pfrzze@gmail.com>
2026-03-10 23:39:47 +0530

    Fix Vercel serverless setup

--- config/db.js
+++ config/db.js
@@ -1,14 +1,28 @@
-import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import studentRoutes from "./routes/studentRoutes.js";
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+dotenv.config();
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
+const app = express();
 
-export default connectDB;
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// connect database
+connectDB();
+
+// home route
+app.get("/", (req, res) => {
+  res.send("Student Management System Running");
+});
+
+// student routes
+app.use("/students", studentRoutes);
+
+// export app for Vercel
+export default app;
